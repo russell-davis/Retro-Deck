2652f8b89009f0d9b4aea05120d6c66c0b333e55
Mark McGookin <mark.mcgookin@gmail.com>
2022-09-09 22:47:25 +0100

    removed unused function

--- src/fileUtilities.py
+++ src/fileUtilities.py
@@ -12,10 +12,3 @@ def file_exists(filename):
         file_exists = False
 
     return file_exists
-
-def get_device_name(file_path):
-    print("opening " + file_path)
-    f = open(file_path)
-    x = f.read()
-    d = json.loads(x)
-    return d["name"]
